5407da50948f452b640d3d5d27318d9b05be6ca8
Harrison Price <hprice518@gmail.com>
2020-04-27 00:48:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,8 +108,7 @@ def changing_alan
 end
 
 def adding_to_dennis
-	programmer_hash = 
- 		{
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -123,5 +122,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash
 end
